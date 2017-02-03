511a7e0e48102d3114043374317f24d5ee6e2b43
Ray-E-Artz <ray.e.artz@gmail.com>
2017-02-03 13:49:37 -0600

    added ProductsListComponent

---
+++ src/app/product.model.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Ray on 2/3/17.
+ */
+/**
+ * Provides a `Product` object
+ */
+export class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number
+  ){}
+}
