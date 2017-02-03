c23ae553f61ebe1b4553f4ecf8f30f6d878723d5
Ray-E-Artz <ray.e.artz@gmail.com>
2017-02-03 14:55:07 -0600

    added ProductImageComponent

--- src/app/product-row/product-row.component.ts
+++ src/app/product-row/product-row.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from "../product.model";
+/**
+ * @ProductRow: A component for the view of single Product
+ */
 
 @Component({
   selector: 'app-product-row',
   templateUrl: './product-row.component.html',
-  styleUrls: ['./product-row.component.css']
+  styleUrls: ['./product-row.component.css'],
+  host: {'class': 'item'}
 })
 export class ProductRowComponent implements OnInit {
+  @Input() product: Product;
 
   constructor() { }
 
