15f735ad9506124f8eb4dd2695abb169825feeba
Ray-E-Artz <ray.e.artz@gmail.com>
2017-02-03 18:30:51 -0600

    display looks okay; click is not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,27 @@ import {Product} from './product.model';
 })
 export class AppComponent {
   products: Product[];
+  price: number;
 
   constructor() {
+    this.price = 3;
     this.products = [
       new Product(
         'MYSHOES',
         'Black Running Shoes',
-        '/resources/images/products/black-shoes.jpg',
+        './assets/images/products/black-shoes.jpg',
         ['Men', 'Shoes', 'Running Shoes'],
         109.99),
       new Product(
         'NEATOJACKET',
         'Blue Jacket',
-        '/resources/images/products/blue-jacket.jpg',
+        './assets/images/products/blue-jacket.jpg',
         ['Women', 'Apparel', 'Jackets & Vests'],
         238.99),
       new Product(
         'NICEHAT',
         'A Nice Black Hat',
-        '/resources/images/products/black-hat.jpg',
+        './assets/images/products/black-hat.jpg',
         ['Men', 'Accessories', 'Hats'],
         29.99)
     ];
